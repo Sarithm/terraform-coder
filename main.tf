c686217e0856082d89054cc3c9adffe8b26f9085
Samson Gudise <samson.gudise@stem.com>
2020-11-25 19:48:16 -0700

    test message

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ terraform {
         key = "aws-infra/terraform-state/terraform.tfstate"
         region = "us-west-2"
         encrypt = true
-        dynamodb_table = "terraform-state-lock"
+        #dynamodb_table = "terraform-state-lock"
     }
 }
     
