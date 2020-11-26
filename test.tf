948870ab8dfcf18ed32732a95df47da46ebc3d40
Samson Gudise <samson.gudise@stem.com>
2020-11-25 20:51:06 -0700

    test message

--- test.tf
+++ test.tf
@@ -11,5 +11,3 @@ output "caller_arn" {
 output "caller_user" {
   value = data.aws_caller_identity.current.user_id
 }
-
-terraform plan -input=false -refresh -no-color -out \"/home/atlantis/.atlantis/repos/Sarithm/terraform-coder/2/mgmt/terraform-coder-mgmt.tfplan\" -lock false" in "/home/atlantis/.atlantis/repos/Sarithm/terraform-coder/2/mgmt": exit status 1
